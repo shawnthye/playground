f7ac470369a24a097b322bed376beb594fb88776
shawnthye <shawn.thye@gmail.com>
2021-11-10 03:00:29 +0800

    [WIP]

--- core-ui/src/main/java/core/playground/ui/ActivityExtension.kt
+++ core-ui/src/main/java/core/playground/ui/ActivityExtension.kt
@@ -2,8 +2,13 @@ package core.playground.ui
 
 import androidx.activity.ComponentActivity
 import androidx.databinding.ViewDataBinding
+import androidx.viewbinding.ViewBinding
 
 fun ComponentActivity.setContentView(binding: ViewDataBinding) {
     binding.lifecycleOwner = this
+    setContentView(binding.root)
+}
+
+fun ComponentActivity.setContentView(binding: ViewBinding) {
     setContentView(binding.root)
 }
